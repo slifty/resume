23bbec59e6be310bb35fd096405cbba8139e1faf
Dan Schultz <slifty@gmail.com>
2025-01-16 17:12:22 -0500

    Initial setup
    This setup creates a Svelte + Vite site which gets prettified, linted,
    and published.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+	<link rel="stylesheet" href="/main.css" />
+	<meta
+		name="description"
+		content="Dan is just this guy, you know?"
+	/>
+	<meta property="og:type" content="website" />
+	<meta
+		property="og:image"
+		content=""
+	/>
+	<meta
+		property="og:description"
+		content="Dan is just this guy, you know?"
+	/>
+</svelte:head>
+
+<div id="header">
+	<div id="logo">
+		Header
+	</div>
+</div>
+
+<div id="content">
+	<slot />
+</div>
+
+<div id="footer">
+	<em>This site is Open Source.</em> Want to make an improvement?
+	<a href="https://github.com/slifty/resume/">Open a PR</a>.
+</div>
+
+<style>
+</style>
